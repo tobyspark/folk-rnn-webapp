0f26499b03028a73197944cc872984fad168a1e0
Toby Harris <operator@tobyz.net>
2017-10-23 18:12:59 +0100

    Generating placeholder job specs, composing and saving the results

--- folk_rnn_task/constants.py
+++ folk_rnn_task/constants.py
@@ -1,1 +1,7 @@
+from os import path
+STORE_PATH = '/var/opt/folk_rnn_task'
+
+MODEL_PATH = path.join(STORE_PATH, 'models')
+TUNE_PATH = path.join(STORE_PATH, 'tunes')
+
 POLL_SECS = 2 # The minimum polling frequency to check for new jobs.
--- folk_rnn_task/folk_rnn_task.py
+++ folk_rnn_task/folk_rnn_task.py
@@ -1,25 +1,62 @@
 #!/usr/bin/python
 
 from __future__ import print_function
-from constants import POLL_SECS
+from constants import TUNE_PATH, MODEL_PATH, POLL_SECS
 from folk_rnn import Folk_RNN
 import random
 import time
+import os
+import cPickle as pickle
 
-def register_new_job():
-    return random.choice([False, False, False, False, True])
+def get_new_job():
+    if random.choice([False, False, False, False, True]):
+        return new_jobber.next()
 
-def process_job():
-    time.sleep(5)
+def new_job_generator():
+    id_counter = 0
+    model_path = os.path.join(MODEL_PATH, 'test_model.pickle_2')
+    with open(model_path, "r") as f:
+        model = pickle.load(f)
+    while True:
+        job_spec = dict(model)
+        job_spec['id'] = id_counter
+        job_spec['temperature'] = None
+        job_spec['seed'] = None
+        id_counter += 1
+        yield job_spec
+
+def process_job(job_spec):
+    folk_rnn = Folk_RNN(
+        job_spec['token2idx'],
+        job_spec['param_values'], 
+        job_spec['num_layers'], 
+        job_spec['rnn_size'],
+        job_spec['grad_clipping'],
+        job_spec['dropout'], 
+        job_spec['embedding_size'], 
+        # job_spec['rng_seed'], 
+        # job_spec['temperature'],
+        )
+    folk_rnn.seed_tune(job_spec['seed'])
+    tune = folk_rnn.compose_tune()
+    tune_path = os.path.join(TUNE_PATH, 'test_tune_{}'.format(job_spec['id']))
+    with open(tune_path, 'w') as f:
+        f.write(tune)
     return True
-    
+
+try:
+    os.makedirs(TUNE_PATH)
+except:
+    pass
+new_jobber = new_job_generator()
 while True:
     print('Loop start at {}'.format(time.ctime()))
     loop_start_time = time.time()
     
-    if register_new_job():
+    new_job = get_new_job()
+    if new_job:
         print('Processing...')
-        process_job()
+        process_job(new_job)
     
     elapsed_secs = time.time() - loop_start_time
     if elapsed_secs < POLL_SECS:
