744203a36a40a7d74c39248e5657178e9e52186d
Toby Harris <operator@tobyz.net>
2017-10-24 18:07:27 +0100

    Functional test comments for minimum viable app

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Ada navigates to the folk_rnn web app
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# Sees that it is indeed about the folk-rnn folk music style modelling project
+assert 'Folk RNN' in browser.title
+
+# Sees a compose tune section at the top of the page, hits "compose"
+
+# Compose section changes to "composition in process"
+
+# Eventually, the compose section changes again, displaying the tune in ABC notation.
+# There is also a button to reset and compose again. Which she presses, and the 
+# compose section is as it was on first load.
+
+browser.quit()
