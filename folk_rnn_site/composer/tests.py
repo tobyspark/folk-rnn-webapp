31eba325157adc30de619ccaf551db3999493fe4
Toby Harris <operator@tobyz.net>
2017-10-31 16:27:23 +0000

    composer_page returns minimal HTML

--- folk_rnn_site/composer/tests.py
+++ folk_rnn_site/composer/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from composer.views import composer_page
 
 class HomePageTest(TestCase):
@@ -7,4 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_composer_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, composer_page)
-        
+    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = composer_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Folk_RNN Composer</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- folk_rnn_site/composer/views.py
+++ folk_rnn_site/composer/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def composer_page():
-    pass
+def composer_page(request):
+    return HttpResponse('<html><title>Folk_RNN Composer</title></html>')
