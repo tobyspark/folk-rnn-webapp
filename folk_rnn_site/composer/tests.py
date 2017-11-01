ae1d9d7faf8826dbf792b2ffdf32b660f6d2596c
Toby Harris <operator@tobyz.net>
2017-11-01 14:10:42 +0000

    Tune model

--- folk_rnn_site/composer/tests.py
+++ folk_rnn_site/composer/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from django.http import HttpRequest
 
 from composer.views import composer_page
+from composer.models import Tune
 
 class HomePageTest(TestCase):
     
@@ -14,3 +15,23 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'seed_text': 'some ABC notation'})
         self.assertTemplateUsed(response, 'compose.html')
         self.assertIn('some ABC notation', response.content.decode())
+
+class TuneModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        first_tune = Tune()
+        first_tune.seed = 'ABC'
+        first_tune.save()
+        
+        second_tune = Tune()
+        second_tune.seed = 'DEF'
+        second_tune.save()
+        
+        saved_tunes = Tune.objects.all()
+        self.assertEqual(saved_tunes.count(), 2)
+        
+        first_saved_tune = saved_tunes[0]
+        second_saved_tune = saved_tunes[1]
+        self.assertEqual(first_saved_tune.seed, 'ABC')
+        self.assertEqual(second_saved_tune.seed, 'DEF')
+        
