837e9c095cc73d9078bbd4853287afed218648ea
Toby Harris <operator@tobyz.net>
2017-11-01 13:16:24 +0000

    displays posted seed

--- folk_rnn_site/composer/tests.py
+++ folk_rnn_site/composer/tests.py
@@ -9,3 +9,8 @@ class HomePageTest(TestCase):
     def test_compose_page_uses_compose_template(self):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'compose.html')
+    
+    def test_compose_page_saves_seed_on_POST(self):
+        response = self.client.post('/', data={'seed_text': 'some ABC notation'})
+        self.assertTemplateUsed(response, 'compose.html')
+        self.assertIn('some ABC notation', response.content.decode())
--- folk_rnn_site/composer/views.py
+++ folk_rnn_site/composer/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def composer_page(request):
+    if request.method == 'POST':
+        return render(request, 'compose.html', {
+            'seed_text': request.POST['seed_text'],
+        })
     return render(request, 'compose.html')
