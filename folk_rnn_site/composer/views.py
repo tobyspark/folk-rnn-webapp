7df37ff882c7a9d6c404e011e4c5c4019aa95b8e
Toby Harris <operator@tobyz.net>
2017-11-22 15:22:39 +0000

    Only show save button for 'Your development'

--- folk_rnn_site/composer/views.py
+++ folk_rnn_site/composer/views.py
@@ -43,7 +43,7 @@ def candidate_tune_page(request, tune_id=None):
             'rnn_has_started': True,
             })
     
-    show_rnn = False
+    show_user = True
     if request.method == 'POST':
         form = CandidateForm(request.POST)
         if form.is_valid():
@@ -51,20 +51,20 @@ def candidate_tune_page(request, tune_id=None):
                 tune.user_tune = form.cleaned_data['tune']
                 tune.save()
             if form.cleaned_data['edit'] == 'rnn':
-                show_rnn = True
-    if show_rnn:
+                show_user = False
+    if show_user:
+        form = CandidateForm({
+                    'tune': tune.user_tune if tune.user_tune else tune.rnn_tune,
+                    'edit': 'user',
+                    'edit_state': 'user',
+                    })
+    else:
         form = CandidateForm({
                     'tune': tune.rnn_tune,
                     'edit': 'rnn',
                     'edit_state': 'rnn',
                     })
         form.fields['tune'].widget.attrs['readonly'] = True
-    else:
-        form = CandidateForm({
-                    'tune': tune.user_tune if tune.user_tune else tune.rnn_tune,
-                    'edit': 'user',
-                    'edit_state': 'user',
-                    })
 
     return render(request, 'candidate-tune.html', {
         'model': tune.rnn_model_name,
@@ -74,4 +74,5 @@ def candidate_tune_page(request, tune_id=None):
         'requested': tune.requested,
         'rnn_duration': (tune.rnn_finished - tune.rnn_started).total_seconds(),
         'form': form,
+        'show_user': show_user,
         })
