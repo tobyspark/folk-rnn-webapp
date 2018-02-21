59bd199863a5f76674626b2217a3a9dd87c5309f
Toby Harris <operator@tobyz.net>
2018-02-21 01:48:01 +0000

    Provision: create models for three metadata pickles

--- tools/create_model_from_config_meta.py
+++ tools/create_model_from_config_meta.py
@@ -6,26 +6,32 @@ import os
 import importlib
 import pickle
 
+metadata_paths = [
+        ('/folk_rnn/metadata/config5-wrepeats-20160112-222521.pkl', 'with_repeats'),
+        ('/folk_rnn/metadata/config5-worepeats-20160311-134539.pkl', 'without_repeats'),
+        ('/folk_rnn/metadata/config5_resume-allabcworepeats_parsed_Tallis_trimmed1000-20171228-191847_epoch39.pkl', 'without_repeats_tallis'),
+        ]
+
 config_module = 'configurations.config5'
-metadata_path = '/folk_rnn/metadata/config5-wrepeats-20160112-222521.pkl'
-model_dir = '/var/opt/folk_rnn_task/models/'
-
 config = importlib.import_module(config_module, package='folk_rnn')
-with open(metadata_path, 'rb') as f:
-    metadata = pickle.load(f, encoding='latin1') # latin1 maps 0-255 to unicode 0-255
-    
-model = {
-    'token2idx': metadata['token2idx'],
-    'param_values': metadata['param_values'], 
-    'num_layers': config.num_layers, 
-    'rng_seed': None, 
-    'temperature': None,
-}
 
+model_dir = '/var/opt/folk_rnn_task/models/'
 try:
     os.makedirs(model_dir)
 except:
     pass
-path = os.path.join(model_dir, 'test_model.pickle')
-with open(path, 'wb') as f:
-    pickle.dump(model, f)
+
+for metadata_path, model_name in metadata_paths: 
+    with open(metadata_path, 'rb') as f:
+        metadata = pickle.load(f, encoding='latin1') # latin1 maps 0-255 to unicode 0-255
+        
+    model = {
+        'token2idx': metadata['token2idx'],
+        'param_values': metadata['param_values'], 
+        'num_layers': config.num_layers, 
+        'metadata_path': metadata_path,
+    }
+    
+    path = os.path.join(model_dir, model_name + '.pickle')
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
