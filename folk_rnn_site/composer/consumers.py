046db9c7997148dbf57c83d7bf7aed1e55075939
Toby Harris <operator@tobyz.net>
2018-02-23 10:49:00 +0000

    ABC generation displayed live, handling connection mid-generation

--- folk_rnn_site/composer/consumers.py
+++ folk_rnn_site/composer/consumers.py
@@ -20,42 +20,58 @@ ABC2ABC_COMMAND = [
 class FolkRNNConsumer(SyncConsumer):
 
     def folkrnn_generate(self, event):
+        '''
+        Generate the tune, pulling parameters from the database, and writing back
+        the result. Will also notify consumers with group 'tune_x' of abc updates 
+        as the generation proceeds, and generation completion.
+        '''
         tune = RNNTune.objects.get(id=event['id'])
         
         tune.rnn_started = datetime.now()
         tune.save()
         
-        def send_token(token):
+        # Machinery to build ABC incrementally, notifying consumers of abc updates.
+        abc = 'X:{id}\nT:Folk RNN Candidate Tune No{id}\n'.format(id=tune.id)
+        token_count = 0
+        def on_token(token):
+            nonlocal token_count, abc
+            token_count += 1
+            if token_count == 1:
+                if token[0:2] != 'M:':
+                    abc += 'M:none\n'
+                else:
+                    abc += token + '\n'
+            elif token_count == 2:
+                if token[0:2] != 'K:':
+                    abc += 'K:none\n'
+                else:
+                    abc += token + '\n'
+            else:
+                abc += token
             async_to_sync(self.channel_layer.group_send)(
                                     'tune_{}'.format(tune.id),
                                     {
-                                        'type': 'send_token',
-                                        'token': token,
+                                        'type': 'update_abc',
+                                        'abc': abc,
                                     })
         
+        # Do the generation
         folk_rnn = folk_rnn_cached(tune.rnn_model_name)
         folk_rnn.seed_tune(tune.prime_tokens if len(tune.prime_tokens) > 0 else None)
         tune_tokens = folk_rnn.generate_tune(
                                     random_number_generator_seed=tune.seed, 
                                     temperature=tune.temp,
-                                    on_token_callback=send_token
+                                    on_token_callback=on_token
                                     )
         
+        # Save out raw folk-rnn output
         tune_path_raw = os.path.join(TUNE_PATH, '{model}_{id}_raw'.format(
                                         model=tune.rnn_model_name.replace('.pickle', ''), 
                                         id=tune.id))
         with open(tune_path_raw, 'w') as f:
             f.write(' '.join(tune_tokens))
         
-        m = tune_tokens.pop(0) if tune_tokens[0][0:2] == 'M:' else 'M:none'
-        k = tune_tokens.pop(0) if tune_tokens[0][0:2] == 'K:' else 'K:none'   
-        abc = 'X:{id}\nT:Folk RNN Candidate Tune No{id}\n{m}\n{k}\n{t}\n'.format(
-                                                        id=tune.id, 
-                                                        m=m, 
-                                                        k=k, 
-                                                        t=''.join(tune_tokens),
-                                                        )
-    
+        # Format the incrementally built ABC
         try:
             abc_bytes = abc.encode()
             result = subprocess.run(
@@ -69,21 +85,24 @@ class FolkRNNConsumer(SyncConsumer):
             print('ABC2ABC failed in folk_rnn_task for id:{}'.format(tune.id))
             return
         
+        # Save the formatted, incrementally built ABC
         tune_path = os.path.join(TUNE_PATH, '{model}_{id}'.format(
                                         model=tune.rnn_model_name.replace('.pickle', ''),
                                         id=tune.id))
         with open(tune_path, 'w') as f:
             f.write(abc)
-    
+        
+        # Save that ABC to the database
         tune.abc = abc
         tune.rnn_finished = datetime.now()
         tune.save()
         
+        # Notify consumers generation has finished
         async_to_sync(self.channel_layer.group_send)(
                                 'tune_{}'.format(tune.id),
                                 {
-                                    'type': 'send_token',
-                                    'token': 'folkrnn_generate_completed',
+                                    'type': 'generation_status',
+                                    'status': 'complete',
                                 })
 
 class ComposerConsumer(JsonWebsocketConsumer):
@@ -91,12 +110,28 @@ class ComposerConsumer(JsonWebsocketConsumer):
     def connect(self):
         self.accept()
         self.tune_id = None
+        self.abc_sent = ''
     
-    def send_token(self, message):
-        print('send_token: {}'.format(message))
+    def update_abc(self, message):
+        '''
+        Send unsent tokens to the client, i.e. realtime update of generation.
+        A websocket will typically connect mid-generation, so broadcasting all 
+        the abc internally and then sending only what is new guarantees complete 
+        abc for the client with minimal network overhead or server complexity.
+        '''
+        # Only send unsent tokens. 
+        # 
+        to_send = message['abc'].replace(self.abc_sent, '')
+        self.abc_sent += to_send
         self.send_json({
                     'command': 'add_token',
-                    'token': message['token']
+                    'token': to_send
+                    })
+    
+    def generation_status(self, message):
+        self.send_json({
+                    'command': 'generation_status',
+                    'status': message['status']
                     })
         
     def receive_json(self, content):
