af82a0414ebbe06b104f96c1d3f57aa21294edef
Toby Harris <operator@tobyz.net>
2018-03-02 12:07:46 +0000

    Tests. Setup issue for async tests.

--- folk_rnn_site/composer/tests.py
+++ folk_rnn_site/composer/tests.py
@@ -4,10 +4,10 @@ from datetime import timedelta
 from time import sleep
 
 from folk_rnn_site.tests import ABC_TITLE, ABC_BODY, mint_abc
-
-from composer import TUNE_PATH
-from composer.consumers import FolkRNNConsumer
 from composer.models import RNNTune
+
+def folk_rnn_create_tune(seed=123, temp=0.1, prime_tokens='a b c'):
+    tune = RNNTune.objects.create(rnn_model_name='with_repeats.pickle', seed=seed, temp=temp, meter='M:4/4', key='K:Cmaj', start_abc=prime_tokens)
 
 def folk_rnn_task_start_mock():
     tune = RNNTune.objects.first()
@@ -22,43 +22,11 @@ def folk_rnn_task_end_mock():
     tune.save()
     return tune
 
-class ComposerTestCase(TestCase):
-    
-    def post_tune(self, seed=123, temp=0.1, prime_tokens='a b c'):
-        return self.client.post('/', data={'model': 'with_repeats.pickle', 'seed': seed, 'temp': temp, 'meter':'M:4/4', 'key': 'K:Cmaj', 'start_abc': prime_tokens})
-    
-class HomePageTest(ComposerTestCase):
+class ViewsTest(TestCase):
     
     def test_home_page_uses_home_template(self):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'composer/home.html')
-    
-    def test_compose_page_can_save_a_POST_request(self):
-        self.post_tune()
-        self.assertEqual(RNNTune.objects.count(), 1)
-        new_tune = RNNTune.objects.first()
-        self.assertEqual(new_tune.temp, 0.1)
-        self.assertEqual(new_tune.prime_tokens, 'M:4/4 K:Cmaj a b c')
-  
-    def test_compose_page_does_not_save_an_invalid_POST_request(self):
-        self.post_tune(prime_tokens='slarty bartfast')
-        self.assertEqual(RNNTune.objects.count(), 0)
-        
-        self.post_tune(seed=-1)
-        self.assertEqual(RNNTune.objects.count(), 0)
-        
-        self.post_tune(temp=11)
-        self.assertEqual(RNNTune.objects.count(), 0)          
-    
-    def test_compose_page_redirects_after_POST(self):
-        print(list(RNNTune.objects.all()))
-        response = self.post_tune()
-        print(list(RNNTune.objects.all()))
-        print(RNNTune.objects.first().id)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/tune/1')
-        
-class TunePageTest(ComposerTestCase):
     
     def test_tune_path_with_no_id_fails_gracefully(self):
         response = self.client.get('/tune/')
@@ -67,16 +35,11 @@ class TunePageTest(ComposerTestCase):
     def test_tune_path_with_invalid_id_fails_gracefully(self):
         response = self.client.get('/tune/1')
         self.assertEqual(response['location'], '/')
-        
-    def test_tune_page_shows_composing_messages(self):
-        self.post_tune()
-        response = self.client.get('/tune/1')
-        self.assertTemplateUsed(response, 'composer/tune-in-process.html')
 
     def test_tune_page_shows_tune(self):
-        self.post_tune()
+        folk_rnn_create_tune()
         folk_rnn_task_start_mock()
-        tune = folk_rnn_task_end_mock()
+        folk_rnn_task_end_mock()
         
         response = self.client.get('/tune/1')
         self.assertTemplateUsed(response, 'composer/tune.html')
@@ -85,11 +48,11 @@ class TunePageTest(ComposerTestCase):
         self.assertContains(response,'with_repeats.pickle')
         self.assertContains(response,'123')
         self.assertContains(response,'0.1')
-        self.assertContains(response,'M:4/4 K:Cmaj a b c')
+        self.assertContains(response, mint_abc())
         # Testing date is beyond the remit of datetime.strttime(), e.g. day of the week without leading zero.
     
     def test_tune_page_can_save_a_POST_request(self):
-        self.post_tune()
+        tune = RNNTune.objects.create()
         folk_rnn_task_start_mock()
         folk_rnn_task_end_mock()
         
--- folk_rnn_site/composer/tests_async.py
+++ folk_rnn_site/composer/tests_async.py
@@ -1,8 +1,18 @@
 import pytest # Channels consumer require tests to be run with `pytest`
-from channels.testing import ApplicationCommunicator
+import json
+from channels.testing import ApplicationCommunicator, WebsocketCommunicator
+from channels.layers import get_channel_layer
 from datetime import timedelta
 
+from composer import TUNE_PATH
+from composer.consumers import FolkRNNConsumer, ComposerConsumer
+from composer.models import RNNTune
 from folk_rnn_site.tests import FOLKRNN_IN, FOLKRNN_OUT, FOLKRNN_OUT_RAW
+
+# FIXME: THERE IS SOMETHING WRONG WITH THE SETUP OF THESE TESTS. 
+# THEY ARE NOT ISOLATED FROM EACH OTHER.
+# e.g. needing to use count_before = RNNTune.objects.count()
+# e.g. line 70 needs a big timeout, and then gives you the ABC from the actual generation task!
 
 @pytest.mark.django_db(transaction=True)  
 @pytest.mark.asyncio
@@ -17,7 +27,7 @@ async def test_folkrnn_consumer():
         'type': 'folkrnn.generate', 
         'id': 1,
     })
-    await communicator.wait(timeout=3)
+    await communicator.wait(timeout=5)
 
     with open(TUNE_PATH + '/with_repeats_1_raw') as f:
         assert f.read() == FOLKRNN_OUT_RAW
@@ -25,9 +35,173 @@ async def test_folkrnn_consumer():
     with open(TUNE_PATH + '/with_repeats_1') as f:
         assert f.read() == FOLKRNN_OUT
 
-    tune = RNNTune.objects.first()
+    tune = RNNTune.objects.last()
     assert tune.rnn_started is not None
     assert tune.rnn_started is not None
     assert tune.rnn_started < tune.rnn_finished
     assert tune.rnn_finished - tune.rnn_started < timedelta(seconds=5)
     assert tune.abc == FOLKRNN_OUT
+
+@pytest.mark.xfail    
+@pytest.mark.django_db(transaction=True)     
+@pytest.mark.asyncio
+async def test_generation_status():
+
+    communicator = WebsocketCommunicator(ComposerConsumer, '/')
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    
+    channel_layer = get_channel_layer()
+
+    # Register. This should add the consumer to the tune groups
+    await communicator.send_to(json.dumps({
+        'command': 'register_for_tune',
+        'tune_id': 1,
+        }))
+    await communicator.send_to(json.dumps({
+        'command': 'register_for_tune',
+        'tune_id': 2,
+        }))
+    
+    # Send the consumer complete ABC via tune group. 
+    # Check it gets back incremental ABC.
+    # And do this for two tunes, to check isolation.
+    await channel_layer.group_send('tune_1', {
+        'type': 'generation_status',
+        'status': 'new_abc',
+        'abc': 'a b c',
+        'tune_id': 1,
+        })
+    response = await communicator.receive_from(timeout=5)
+    assert json.loads(response) == {
+        'command': 'add_token',
+        'token': 'a b c',
+        'tune_id': 1,
+    }
+    
+    await channel_layer.group_send('tune_2', {
+        'type': 'generation_status',
+        'status': 'new_abc',
+        'abc': 'A B C',
+        'tune_id': 2,
+        })
+    response = await communicator.receive_from()
+    assert json.loads(response) == {
+        'command': 'add_token',
+        'token': 'A B C',
+        'tune_id': 2,
+    }
+
+    await channel_layer.group_send('tune_1', {
+        'type': 'generation_status',
+        'status': 'new_abc',
+        'abc': 'a b c d e f',
+        'tune_id': 1,
+        })
+    response = await communicator.receive_from()
+    assert json.loads(response) == {
+        'command': 'add_token',
+        'token': 'd e f',
+        'tune_id': 1,
+    }
+    
+    await channel_layer.group_send('tune_2', {
+        'type': 'generation_status',
+        'status': 'new_abc',
+        'abc': 'A B C D E F',
+        'tune_id': 2,
+        })
+    response = await communicator.receive_from()
+    assert json.loads(response) == {
+        'command': 'add_token',
+        'token': 'D E F',
+        'tune_id': 2,
+    }
+    
+    await communicator.disconnect()
+
+@pytest.mark.django_db(transaction=True)    
+@pytest.mark.asyncio
+async def test_receive_json_compose_valid():
+    communicator = WebsocketCommunicator(ComposerConsumer, '/')
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    
+    content = {
+        'command': 'compose',
+        'data': {
+            'model': 'with_repeats.pickle',
+            'temp': 0.1,
+            'seed': 123,
+            'meter': 'M:4/4',
+            'key': 'K:Cmaj',
+            'start_abc': 'a b c',
+            }
+    }
+    await communicator.send_to(json.dumps(content))
+    response = await communicator.receive_from()
+    
+    tune = RNNTune.objects.last()
+    assert json.loads(response) == {
+        'command': 'add_tune',
+        'tune_id': tune.id
+    }
+    assert tune.rnn_model_name == 'with_repeats.pickle'
+    assert tune.temp == 0.1
+    assert tune.seed == 123
+    assert tune.prime_tokens == 'M:4/4 K:Cmaj a b c'
+    await communicator.disconnect()
+    
+@pytest.mark.django_db(transaction=True)    
+@pytest.mark.asyncio
+async def test_receive_json_compose_invalid():
+    communicator = WebsocketCommunicator(ComposerConsumer, '/')
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    
+    count_before = RNNTune.objects.count()
+
+    content = {
+        'command': 'compose',
+        'data': {
+            'model': 'with_repeats.pickle',
+            'temp': 0.1,
+            'seed': -1,
+            'meter': 'M:4/4',
+            'key': 'K:Cmaj',
+            'start_abc': 'slarty bartfast',
+            }
+    }
+    await communicator.send_to(json.dumps(content))
+    assert count_before == RNNTune.objects.count()
+
+    content = {
+        'command': 'compose',
+        'data': {
+            'model': 'with_repeats.pickle',
+            'temp': 0.1,
+            'seed': -1,
+            'meter': 'M:4/4',
+            'key': 'K:Cmaj',
+            'start_abc': 'a b c',
+            }
+    }
+    await communicator.send_to(json.dumps(content))
+    assert count_before == RNNTune.objects.count()
+    
+    content = {
+        'command': 'compose',
+        'data': {
+            'model': 'with_repeats.pickle',
+            'temp': 11,
+            'seed': 123,
+            'meter': 'M:4/4',
+            'key': 'K:Cmaj',
+            'start_abc': 'a b c',
+            }
+    }
+    await communicator.send_to(json.dumps(content))
+    assert count_before == RNNTune.objects.count()
+    
+    await communicator.disconnect()
+    
