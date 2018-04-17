efe78f994a494db737fb796e782e5aa72e435a88
Toby Harris <operator@tobyz.net>
2018-04-17 11:42:06 +0100

    signup

--- folk_rnn_site/archiver/forms.py
+++ folk_rnn_site/archiver/forms.py
@@ -17,3 +17,8 @@ class TuneForm(forms.Form):
 class CommentForm(forms.Form):
     text = forms.CharField(label='Comment:', widget=forms.Textarea(attrs={'id': 'new_comment'}))
     author = forms.CharField(label='Author:', widget=forms.TextInput(attrs={'id': 'new_comment_author'}))
+    
+class SignupForm(forms.Form):
+    name = forms.CharField(label='Name:')
+    password = forms.CharField(label='Password:', widget=forms.PasswordInput())
+    email = forms.EmailField(label='Email:')
