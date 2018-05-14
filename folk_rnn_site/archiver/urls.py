502951ec59936305457a84b42d60ae719913a8a5
Toby Harris <operator@tobyz.net>
2018-05-14 19:12:17 +0100

    /tune/x/download/setting/y

--- folk_rnn_site/archiver/urls.py
+++ folk_rnn_site/archiver/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     url(r'^tunes/$', views.tunes_page, name='tunes'),
     url(r'^tune/(?P<tune_id>[0-9]+)$', views.tune_page, name='tune'),
     url(r'^tune/(?P<tune_id>[0-9]+)/download$', views.tune_download, name='tune_download'),
+    url(r'^tune/(?P<tune_id>[0-9]+)/download/setting/(?P<setting_id>[0-9]+)$', views.setting_download, name='setting_download'),
     url(r'^dataset$', views.dataset_download),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^signup', views.signup, name='signup')
