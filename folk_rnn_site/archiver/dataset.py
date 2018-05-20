cbe348159242ad49d750eecd554b275ec0b8144a
Toby Harris <operator@tobyz.net>
2018-05-20 13:23:32 +0100

    dataset download updated for current models

--- folk_rnn_site/archiver/dataset.py
+++ folk_rnn_site/archiver/dataset.py
@@ -3,33 +3,47 @@ import csv
 
 from archiver.models import Tune, Setting
 
-SettingDatum = namedtuple('SettingDatum', [
-                            'id', 
+Datum = namedtuple('SettingDatum', [
+                            'tune_id',
+                            'setting_id', 
                             'name', 
                             'abc',
                             'meter',
                             'key',
-                            'tune_id', 
                             'rnn_model', 
                             'rnn_temperature', 
                             'rnn_seed', 
                             'rnn_prime_tokens',
                             ])
 
+def tune_dataset():
+    return (Datum(tune_id=x.id,
+                setting_id='',
+                name=x.title,
+                abc=x.abc,
+                meter=x.header_m,
+                key=x.header_k,
+                rnn_model=x.rnn_tune.rnn_model_name if x.rnn_tune else '',
+                rnn_temperature=x.rnn_tune.temp if x.rnn_tune else '',
+                rnn_seed=x.rnn_tune.seed  if x.rnn_tune else '',
+                rnn_prime_tokens=x.rnn_tune.prime_tokens  if x.rnn_tune else '',
+            ) for x in Tune.objects.all())
+
 def setting_dataset():
-    return (SettingDatum(id=x.id,
+    return (Datum(tune_id=x.tune.id,
+                        setting_id=x.id,
                         name=x.title,
                         abc=x.abc,
                         meter=x.header_m,
                         key=x.header_k,
-                        tune_id=x.tune.id,
-                        rnn_model=x.tune.rnn_tune.rnn_model_name,
-                        rnn_temperature=x.tune.rnn_tune.temp,
-                        rnn_seed=x.tune.rnn_tune.seed,
-                        rnn_prime_tokens=x.tune.rnn_tune.prime_tokens,
+                        rnn_model=x.tune.rnn_tune.rnn_model_name if x.tune.rnn_tune else '',
+                        rnn_temperature=x.tune.rnn_tune.temp if x.tune.rnn_tune else '',
+                        rnn_seed=x.tune.rnn_tune.seed if x.tune.rnn_tune else '',
+                        rnn_prime_tokens=x.tune.rnn_tune.prime_tokens if x.tune.rnn_tune else '',
                     ) for x in Setting.objects.all())
     
 def dataset_as_csv(f):
     setting_writer = csv.writer(f)
-    setting_writer.writerow(SettingDatum._fields)
+    setting_writer.writerow(Datum._fields)
+    setting_writer.writerows(tune_dataset())
     setting_writer.writerows(setting_dataset())
