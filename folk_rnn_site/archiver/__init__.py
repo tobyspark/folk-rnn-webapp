e9415a6b81adf95de4b0a3a109a36e9b05a02050
Toby Harris <operator@tobyz.net>
2018-06-20 16:40:10 +0100

    algorithmic nerdery

--- folk_rnn_site/archiver/__init__.py
+++ folk_rnn_site/archiver/__init__.py
@@ -3,3 +3,17 @@ from datetime import date
 MAX_RECENT_ITEMS = 5
 TUNE_PREVIEWS_PER_PAGE = 10
 YEAR_CHOICES = range(date.today().year, date.today().year-20, -1)
+
+
+
+
+# algorithmic nerdery
+# modified from https://stackoverflow.com/questions/352670/weighted-random-selection-with-and-without-replacement
+import heapq
+import math
+import random
+
+def weightedSelectionWithoutReplacement(seq, weights, k=1):
+    assert len(seq) == len(weights) > 0
+    elt = [(math.log(random.random()) / weights[i], i) for i in range(len(weights))]
+    return [seq[x[1]] for x in heapq.nlargest(k, elt)]
