d401062c7a218d4c9f1135df969d432a17040138
Toby Harris <operator@tobyz.net>
2018-07-03 16:06:33 +0100

    ABCModel tidy-up

--- folk_rnn_site/folk_rnn_site/models.py
+++ folk_rnn_site/folk_rnn_site/models.py
@@ -4,7 +4,7 @@ import subprocess
 import re
 
 from composer import ABC2ABC_PATH
-header_x_regex = re.compile(r'X:\s*([0-9]+)\s*\n')
+header_x_regex = re.compile(r'X:\s*(\d+)\s*\n')
 header_t_regex = re.compile(r'T:\s*(.*?)\s*\n')
 header_s_regex = re.compile(r'S:\s*(.*?)\s*\n')
 header_f_regex = re.compile(r'F:\s*(.*?)\s*\n')
@@ -13,15 +13,20 @@ header_k_regex = re.compile(r'K:\s*(.*?)\s*\n')
 body_regex = re.compile(r'(K:.*?\n)(.*)',re.DOTALL) # FIXME: also ignore any final /n
 body_four_bars_regex = re.compile(r'(.*?[^|]\|){4}')
 
-def conform_abc(abc, raise_if_invalid=True):
+def conform_abc(abc):
+    '''
+    Attempt to conform ABC as per standard.
+    Raises exception on detecting invalid ABC.
+    Uses command-line tool `abc2abc`.
+    '''
     # Add X if missing; needed for abc2abc
     if not header_x_regex.search(abc):
         abc = 'X:0\n' + abc
     # Verify K, M exist; needed for abc2abc (otherwise will comment out and pass the string)
     if not header_m_regex.search(abc):
-        raise AttributeError('Missing M header')
+        raise AttributeError('Missing M information field, e.g. M:4/4')
     if not header_k_regex.search(abc):
-        raise AttributeError('Missing K header')
+        raise AttributeError('Missing K information field, e.g. K:Cmaj')
     # Parse through abc2abc
     try:
         abc_safe = unidecode(abc) # convert smart quotes to ASCII straight quotes etc.
@@ -34,11 +39,16 @@ def conform_abc(abc, raise_if_invalid=True):
         for line in result.stdout.splitlines():
             if line[:pos] == prefix:
                 errors.append(line[pos:].decode())
-    if errors and raise_if_invalid:
+    if errors:
         raise AttributeError('Invalid ABC: ' + ', '.join(errors))
     return result.stdout.decode()
 
 class ABCModel(models.Model):
+    '''
+    An abstract base class to provide ABC related functionality
+    Assigning None or '' to an information field will remove it.
+    Note only single information fields of each type are handled. 
+    '''
     class Meta:
         abstract = True
         
@@ -46,29 +56,27 @@ class ABCModel(models.Model):
 
     @property
     def title(self):
+        '''
+        ABC T: information field; the tune title
+        '''
         match = header_t_regex.search(self.abc)
         return match.group(1) if match else ''
 
     @title.setter
     def title(self, value):
-        # is value empty? remove the T: line
-        if value == None or value == "":
-            self.abc = header_t_regex.sub('', self.abc)
-        else:
-            if header_t_regex.search(self.abc):
-                # update T: value
-                self.abc = header_t_regex.sub(f'T:{value}\n', self.abc)
-            else:
-                # add T: line after X:
-                self.abc = header_x_regex.sub(f'X:{self.header_x}\nT:{value}\n', self.abc)
+        if header_t_regex.search(self.abc):
+            sub = f'T:{value}\n' if value else ''
+            self.abc = header_t_regex.sub(sub, self.abc)
+        elif value:
+            self.abc = header_x_regex.sub(f'X:{self.header_x}\nT:{value}\n', self.abc)
     
     @property
     def body(self):
+        '''
+        ABC body, e.g. what comes after K: information field
+        '''
         match = body_regex.search(self.abc)
-        try:
-            return match.group(2)
-        except:
-            raise AttributeError(self.abc)
+        return match.group(2) if match else None
     
     @body.setter
     def body(self, value):
@@ -76,61 +84,69 @@ class ABCModel(models.Model):
 
     @property
     def header_x(self):
+        '''
+        ABC X: information field; the reference number
+        '''
         match = header_x_regex.search(self.abc)
-        return match.group(1)
+        return match.group(1) if match else None
 
     @header_x.setter
     def header_x(self, value):
+        # Note value test here has to be true for X=0
         if header_x_regex.search(self.abc):
-            # update X: value
-            self.abc = header_x_regex.sub(f'X:{value}\n', self.abc)
-        else:
-            # add X: line at beginning
+            sub = f'X:{value}\n' if value != None and value != '' else ''
+            self.abc = header_x_regex.sub(sub, self.abc)
+        elif value != None and value != '':
             self.abc = f'X: {value}\n{self.abc}'
     
     @property
     def header_s(self):
-        match = header_s_regex.search(self.abc)
-        return match.group(1)
-    
-    @header_s.setter
-    def header_s(self, value):
         '''
         ABC S: information field; the source
-        Multiple S headers are not handled
         '''
+        match = header_s_regex.search(self.abc)
+        return match.group(1) if match else None
+            
+    @header_s.setter
+    def header_s(self, value):
         if header_s_regex.search(self.abc):
-            # update S: value
-            self.abc = header_s_regex.sub(f'S:{value}\n', self.abc)
-        else:
-            # add S: line after X:
+            sub = f'S:{value}\n' if value else ''
+            self.abc = header_s_regex.sub(sub, self.abc)
+        elif value:
             self.abc = header_x_regex.sub(f'X:{self.header_x}\nS:{value}\n', self.abc)
 
     @property
     def header_f(self):
-        match = header_f_regex.search(self.abc)
-        return match.group(1)
-    
-    @header_f.setter
-    def header_f(self, value):
         '''
         ABC F: information field; the file URL
         '''
+        match = header_f_regex.search(self.abc)
+        return match.group(1) if match else None
+            
+    @header_f.setter
+    def header_f(self, value):
         if header_f_regex.search(self.abc):
-            # update F: value
-            self.abc = header_f_regex.sub(f'F:{value}\n', self.abc)
-        else:
-            # add F: line after X:
+            sub = f'F:{value}\n' if value else ''
+            self.abc = header_f_regex.sub(sub, self.abc)
+        elif value:
             self.abc = header_x_regex.sub(f'X:{self.header_x}\nF:{value}\n', self.abc)
     
     @property
     def header_m(self):
-        return header_m_regex.search(self.abc).group(1)
-
+        '''
+        ABC M: information field; the meter
+        '''
+        match = header_m_regex.search(self.abc)
+        return match.group(1) if match else None
+            
     @property
     def header_k(self):
-        return header_k_regex.search(self.abc).group(1)
-    
+        '''
+        ABC K: information field; the key
+        '''
+        match = header_k_regex.search(self.abc)
+        return match.group(1) if match else None
+            
     @property
     def abc_preview(self):
         '''
