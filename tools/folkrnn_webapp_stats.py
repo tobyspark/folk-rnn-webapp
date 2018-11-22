040e8f2f5d9c2b1c928790d7571acae8364354bb
Toby Harris <operator@tobyz.net>
2018-11-22 12:04:34 +0000

    analyse()

--- tools/folkrnn_webapp_stats.py
+++ tools/folkrnn_webapp_stats.py
@@ -5,6 +5,7 @@ import re
 import ast
 from datetime import datetime, timedelta
 from collections import namedtuple, Counter
+from statistics import mean, pstdev
 
 Datum = namedtuple('Datum', ['date', 'session', 'info'])
 generate_keys = ['model', 'temp', 'seed', 'key', 'meter', 'start_abc']
@@ -258,6 +259,38 @@ def session_view(data):
             sessions[datum.session].append(datum.info)
     return sessions
 
+def analyse(data, tunes, sessions):
+    '''
+    We are interested in seeing:
+    - What is the distribution of tunes generated in a session?
+    - How often do users change parameters?
+    - How many generated tunes are downloaded?
+    - Is there a correlation between download and tweaking parameters, as in, Is the probability of download greater if the user tweaked the parameters. (Some users appear to have downloaded two or more times without generating anything new.)
+    - Of the tunes downloaded, are there any common characteristics, like most of the tunes are in 6/8, Cmaj? 
+    '''
+    
+    duration = data[-1].date - data[0].date
+    session_tunes = {k: [info['tune'] for info in v if isinstance(info, dict) and info.get('action') == 'compose'] for k, v in sessions.items()}
+    session_tune_counts = {k: len(v) for k, v in session_tunes.items()}
+    print(f"{len(tunes)} tunes were generated from {len(session_tunes)} sessions over {duration.days} days.")
+    print(f"Distribution: {sorted(Counter(session_tune_counts.values()).items())}")
+    print(f"With mean: {mean(session_tune_counts.values())} and standard deviation: {pstdev(session_tune_counts.values())}")
+    print()
+    
+    generating_session_tunes = {k: v for k, v in session_tunes.items() if v}
+    generating_session_tune_counts = {k: len(v) for k, v in generating_session_tunes.items()}
+    print("A session is more-or-less the use of a unique browser, tracked over time. However the number of sessions with no tunes generated may have little value, as some sessions reported as distinct were identifiably not, and not all visits to the site may have been made in good faith (e.g. bots). Our best approximation of good-faith users are then joining sessions that are identifiably continuations of previous ones, and then discounting any that did not generate a tune.")
+    print(f"Our best approximation of good-faith users generated on average mean: {mean(generating_session_tune_counts.values())} standard deviation: {pstdev(generating_session_tune_counts.values())}.")
+    print("For each tune generated, the frequency of the following generate parameters being used was:")
+    print({k: mean([k in tune for tune in tunes.values()]) for k in generate_keys})
+    print("For each tune generated, the frequency of it being downloaded <TODO: or archived> was:")
+    print({k: mean([k in tune for tune in tunes.values()]) for k in ['download']})
+    print("If the tune had no changes to the generate parameters, the frequency of it being downloaded was:")
+    print({k: mean([k in tune for tune in tunes.values() if set(tune.keys()).intersection(generate_keys) == set()]) for k in ['download']})
+    print("Whereas if the tune did have changes to the generate parameters, the frequency of it being downloaded was:")
+    print({k: mean([k in tune for tune in tunes.values() if set(tune.keys()).intersection(generate_keys) != set()]) for k in ['download']})
+    
+    # TODO: extract ABC properties, requires database access (as does detemining archived)
     
 if __name__ == '__main__':
     
@@ -275,7 +308,7 @@ if __name__ == '__main__':
     for tune, info in tunes.items():
         print(f'tune {tune}: {info}')
         
-    sessions = session_view()
+    sessions = session_view(data)
     for session, info in sessions.items():
         
         print(f'Session {session} ----------')
