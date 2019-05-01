758ad3eb96b99c444f07e33c5df9acaf7374cb24
Toby Harris <operator@tobyz.net>
2019-05-01 15:06:19 +0100

    Refactor and compose analysis text

--- folk_rnn_site/stats/management/commands/stats.py
+++ folk_rnn_site/stats/management/commands/stats.py
@@ -6,6 +6,7 @@ import ast
 from datetime import datetime, date, timedelta
 from collections import namedtuple, Counter, defaultdict
 from statistics import mean, pstdev, median
+from itertools import chain
 
 from matplotlib import pyplot
 
@@ -50,7 +51,9 @@ class Command(BaseCommand):
                 print(entry)
             print()
         
-        analyse_folkrnn(data, tunes, sessions)
+        iterative_tunes = iterative_composition_view(sessions)
+        
+        analyse_folkrnn(data, tunes, sessions, iterative_tunes)
         analyse_machinefolk(data)
         
 
@@ -318,7 +321,92 @@ def session_view(data):
             sessions[datum.session].append(datum.info)
     return sessions
 
-def analyse_folkrnn(data, tunes, sessions):
+def iterative_composition_view(sessions):
+    '''
+    Produce an iterative composition view of the data
+    e.g. which tunes are a tweak (of the parameters) of the prior tune, and how?
+    Return info for each tune, e.g.
+        25203: {'key'}
+        25204: {'key', 'meter', 'rnn_model_name'}
+        25205: {'seed locked'}
+        25206: {'start_abc is excerpt'}
+        25207: {'meter'}
+        25208: {'key', 'rnn_model_name', 'start_abc is excerpt'}
+    '''
+    from composer.models import RNNTune
+
+    def start_abc_is_excerpt(start_abc, tune_abc):
+        '''
+        Is start_abc an excerpt of the previous tune?
+        This defines what passes for an excerpt, handling quirks like ABC reformatting within the UI
+        '''
+        minimum_excerpt_length = 5 # start_abc is a phrase, e.g. longer than ? notes
+
+        remove_abc_formatting_table = str.maketrans('', '', ' \n\r')
+        start_abc = start_abc.translate(remove_abc_formatting_table)
+        tune_abc = tune_abc.translate(remove_abc_formatting_table)
+
+        if len(start_abc) < minimum_excerpt_length:
+            return False
+        return start_abc in tune_abc
+
+    # Comparison of parameters is going to be pair-wise, through sequential tunes within each session
+    # So first assemble a list of each tune sequence
+    tune_id_sequences = []
+    for session, info in sessions.items():
+        tune_id_sequences.append([])
+        for entry in info:
+            # new sequence within session
+            if entry == 'Generation parameters reset':
+                tune_id_sequences.append([])
+                continue
+
+            # new sequence within session set from an archive tune
+            try:
+                if entry.startswith('Generation parameters set from archived tune '):
+                    last_tune_id = int(entry.replace('Generation parameters set from archived tune ', ''))
+                    tune_id_sequences[-1].append(last_tune_id)
+                    continue
+            except:
+                pass
+
+            # compose entry within sequence
+            try:
+                if entry['action'] == 'compose':
+                    tune_id = entry['tune']
+                    if last_tune_id:
+                        tune_id_sequences[-1].append(tune_id)
+                    continue
+            except:
+                pass
+
+    tune_id_sequences = [x for x in tune_id_sequences if len(x) > 1]
+
+    # Next, do the pair-wise comparison, building a set of changed parameters for each tune
+    iterative_tunes = {}   
+    for seq in tune_id_sequences:
+        for prev_tune_id, tune_id in zip(seq, seq[1:]):
+            prev_tune = RNNTune.objects.get(id=prev_tune_id)
+            tune = RNNTune.objects.get(id=tune_id)
+
+            info = set()
+            if prev_tune.rnn_model_name != tune.rnn_model_name: info.add('model')
+            if prev_tune.seed == tune.seed: info.add('seed locked') # default is for this to auto-change     
+            if prev_tune.temp != tune.temp: info.add('temp')
+            if prev_tune.key != tune.key: info.add('key')
+            if prev_tune.meter != tune.meter: info.add('meter')
+            if prev_tune.start_abc != tune.start_abc: 
+                if start_abc_is_excerpt(tune.start_abc, prev_tune.abc): 
+                    info.add('start_abc is excerpt')
+                else:
+                    info.add('start_abc')
+
+            if info:
+                iterative_tunes[tune_id] = info
+
+    return iterative_tunes
+
+def analyse_folkrnn(data, tunes, sessions, iterative_tunes):
     '''
     We are interested in seeing:
     - What is the distribution of tunes generated in a session?
@@ -329,7 +417,7 @@ def analyse_folkrnn(data, tunes, sessions):
     '''
     
     def format_dict(d):
-        return ', '.join([f'{k}: {v:.2}' for k, v in d.items()])
+        return ', '.join(sorted(f'{k}: {v:.2}' for k, v in d.items()))
     
     duration = data[-1].date - data[0].date
     session_tunes = {k: [info['tune'] for info in v if isinstance(info, dict) and info.get('action') == 'compose'] for k, v in sessions.items()}
@@ -355,6 +443,14 @@ def analyse_folkrnn(data, tunes, sessions):
     changes = {k: mean([k in tune for tune in tunes.values() if set(tune.keys()).intersection(generate_keys) != set()]) for k in export_keys}
     changes.update({'all': mean([set(export_keys).intersection(tune.keys()) != set() for tune in tunes.values() if set(tune.keys()).intersection(generate_keys) != set()])})
     print(format_dict(changes))
+    print()
+    
+    print("Refining the session data above, we can analyze only those tracks which are in some way a tweak of the one that came before. These are now metrics of an iterative approach to generation of the tunes, demonstrating co-composition. The frequency of the generate parameters used now becomes:")
+    change_tunes_count = len(iterative_tunes)
+    change_category_counts = Counter(chain(*iterative_tunes.values()))
+    change_category_freqs = {k: v/change_tunes_count for k, v in change_category_counts.items()}
+    print(format_dict(change_category_freqs))
+    print(f"One feature now possible to expose is whether the user has identified a salient phrase in the prior tune, and has primed the generation of the new tune with this phrase. This is the strongest metric of co-composition available. This is reported above as 'start_abc is excerpt', tested for phrases comprising five characters or more (e.g. five notes, or fewer with phrasing). This happened {change_category_counts['start_abc is excerpt']} times; {change_category_freqs['start_abc is excerpt']:.0%}")
     print()
     
     print("Usage over time –")
@@ -397,6 +493,7 @@ def analyse_folkrnn(data, tunes, sessions):
     n = n[2] # analyse tunes generated
     noted_week_proportion = (n[swedish_week] + n[german_week]) / sum(n) # 55% in early Jan 2019 !
     print(f"Usage data over time shows three features. Overall use for the first {split_week} weeks was similar, with a median of {median(n[:split_week])} tunes generated each week. In the subsequent {len(n) - split_week} weeks to the time of writing, overall use increased, with a median of {median(n[split_week:])} tunes generated each week. This period also features usage spikes. One week, correlating to an interview in Swedish media, shows {n[swedish_week] / median(n[split_week:]):.1f}x the median tunes generated. The largest, correlating to a mention in German media, shows an {n[german_week] / median(n[split_week:]):.1f}x increase.")
+    print()
 
 def analyse_machinefolk(data):
     from archiver.models import Tune, Setting, Recording, Event, User, Collection, CollectionEntry
@@ -425,89 +522,6 @@ def analyse_machinefolk(data):
     print(f"Of the {tunes_total} contributed tunes, {tunes_folkrnn} were generated on, and archived from, folkrnn.org. Of these entirely machine-generated tunes, {tunes_folkrnn_settings} have had human edits contributed; themachinefolksession.org currently hosts {settings_folkrnn} settings of folkrnn generated tunes in total.")
     print(f"{tunebooks} Registered users have selected {tunebook_entries} tunes as being noteworthy enough to add to their tunebooks. {tunebook_entries_settings/tunebook_entries:.0%} of these are actually settings of the tune, rather than the original tune. Per the algorithm used by the home page of themachinefolksession.org to surface ‘interesting’ tunes, “{interesting_tune.title}” is the most, with {interesting_tune.setting__count} settings and {interesting_tune.recording__count} recordings.")
     print(f"Most content-affecting activity has been from the administrators, however. Sturm accounts for {actions_bob/actions_total:.0%} of such activity.")
-
-def analyse_folkrnn_iterative_composition(sessions):
-    '''
-    
-    '''
-    from composer.models import RNNTune
-    
-    def start_abc_is_excerpt(start_abc, tune_abc):
-        '''
-        Is start_abc an excerpt of the previous tune?
-        This defines what passes for an excerpt, handling quirks like reformatting within the UI
-        '''
-        minimum_excerpt_length = 5 # start_abc is a phrase, e.g. longer than ? notes
-        
-        remove_abc_formatting_table = str.maketrans('', '', ' \n\r')
-        start_abc = start_abc.translate(remove_abc_formatting_table)
-        tune_abc = tune_abc.translate(remove_abc_formatting_table)
-        
-        if len(start_abc) < minimum_excerpt_length:
-            return False
-        return start_abc in tune_abc
-    
-    tune_id_sequences = []
-    for session, info in sessions.items():
-        tune_id_sequences.append([])
-        for entry in info:
-            # new sequence within session
-            if entry == 'Generation parameters reset':
-                tune_id_sequences.append([])
-                continue
-            
-            # new sequence within session set from an archive tune
-            try:
-                if entry.startswith('Generation parameters set from archived tune '):
-                    last_tune_id = int(entry.replace('Generation parameters set from archived tune ', ''))
-                    tune_id_sequences[-1].append(last_tune_id)
-                    continue
-            except:
-                pass
-            
-            # compose entry within sequence
-            try:
-                if entry['action'] == 'compose':
-                    tune_id = entry['tune']
-                    if last_tune_id:
-                        tune_id_sequences[-1].append(tune_id)
-                    continue
-            except:
-                pass
-    
-    tune_id_sequences = [x for x in tune_id_sequences if len(x) > 1]
-    
-    for seq in tune_id_sequences:
-        print(seq)
-    
-    iterative_tunes = {}   
-    for seq in tune_id_sequences:
-        for prev_tune_id, tune_id in zip(seq, seq[1:]):
-            prev_tune = RNNTune.objects.get(id=prev_tune_id)
-            tune = RNNTune.objects.get(id=tune_id)
-            
-            info = set()
-            if prev_tune.rnn_model_name != tune.rnn_model_name: info.add('rnn_model_name')
-            if prev_tune.seed == tune.seed: info.add('seed locked') # default is for this to auto-change     
-            if prev_tune.temp != tune.temp: info.add('temp')
-            if prev_tune.key != tune.key: info.add('key')
-            if prev_tune.meter != tune.meter: info.add('meter')
-            if prev_tune.start_abc != tune.start_abc: 
-                if start_abc_is_excerpt(tune.start_abc, prev_tune.abc): 
-                    info.add('start_abc is excerpt')
-                else:
-                    info.add('start_abc')
-            
-            if len(info):
-                iterative_tunes[tune_id] = info
-
-                # print(f'     tune: {tune_id}')
-                # print(f'start_abc: "{start_abc}"')
-                # print(f'       in: {previous_tune}')
-                # print()
-    
-    for tune_id, info in iterative_tunes.items():
-        print(tune_id, info)
 
 if __name__ == '__main__':
     
